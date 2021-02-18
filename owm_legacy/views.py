12dcb786b72f3eb54d9d5479ddd8605a63abe85e
Federico Capoano <f.capoano@openwisp.io>
2021-02-18 17:44:59 -0500

    [change] Use update logic from controller
    OW Controller performs adjustments to the IP addresses
    so let's reuse those.

--- owm_legacy/views.py
+++ owm_legacy/views.py
@@ -1,16 +1,15 @@
 import swapper
 from django.shortcuts import get_object_or_404
 
-from openwisp_controller.config.utils import (
-    send_device_config,
-    send_file,
-    update_last_ip,
-)
+from openwisp_controller.config.controller.views import UpdateLastIpMixin
+from openwisp_controller.config.utils import send_device_config, send_file
 
 from .utils import forbid_unallowed
 
 Device = swapper.load_model('config', 'Device')
 Config = swapper.load_model('config', 'Config')
+ip_updater = UpdateLastIpMixin()
+ip_updater.model = Device
 
 
 def get_config_md5(request, mac_address):
@@ -19,7 +18,7 @@ def get_config_md5(request, mac_address):
     """
     forbid_unallowed(request)
     config = get_object_or_404(Config, device__mac_address__iexact=mac_address)
-    update_last_ip(config.device, request)
+    ip_updater.update_last_ip(config.device, request)
     return send_file(mac_address, config.checksum)
 
 
